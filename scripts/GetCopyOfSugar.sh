6a0976fd785e3fcff37340281d92696133c7d3ea
Lauren Schaefer <lschaefer@sugarcrm.com>
2018-03-26 14:34:17 -0400

    Fix Sugar download from community

--- scripts/GetCopyOfSugar.sh
+++ scripts/GetCopyOfSugar.sh
@@ -149,7 +149,7 @@ rm -f $cookieFile
 # Create the cookie file and set the file permissions (required for Travis CI)
 sudo touch $cookieFile
 sudo chmod 777 $cookieFile
-sudo chmod -R 777 $sugarDirectory
+sudo chmod -R 777 .
 
 
 ######################################################################
